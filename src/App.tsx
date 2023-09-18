a742ea3241991a32cc38a3c1795bf37082723e3b
Rick H <rickhellenthal@hotmail.com>
2023-09-18 14:28:15 +0200

    video background and date counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+//@ts-ignore
+import sample from "./assets/background.mp4";
+import { differenceInDays } from "date-fns";
+
 export const App = () => {
+    const result = differenceInDays(new Date(2024, 3, 19), new Date());
+
     return (
-        <div className="h-screen flex justify-center items-center my-custom-bg-class">
-            <div className="w-1/3 h-20 rounded flex justify-center items-center">
+        <div className="h-screen flex justify-center items-center relative">
+            <video className="absolute h-screen w-screen object-cover" autoPlay loop muted>
+                <source src={sample} type="video/mp4" />
+            </video>
+
+            <div className="h-20 rounded flex justify-center items-center z-20 w-1/3 md:w-full flex-col">
                 <h1 className="text-6xl text-slate-50 select-none">Bruiloft Eef & JW</h1>
+                <h2 className="text-4xl text-slate-50 select-none mt-2">over {result} dagen</h2>
             </div>
         </div>
     );
