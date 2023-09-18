26ac4559e5d8f498aba8a1d67c63282e239f662e
Rick H <rickhellenthal@hotmail.com>
2023-09-18 15:44:30 +0200

    Even more fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,26 @@ import "./App.css";
 //@ts-ignore
 import sample from "./assets/background1080.mp4";
 import { differenceInDays } from "date-fns";
+import { useEffect } from "react";
 
 export const App = () => {
+    const setViewHeight = () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            setViewHeight();
+        });
+
+        setViewHeight();
+    }, []);
+
     const result = differenceInDays(new Date(2024, 3, 19), new Date());
     return (
         <>
-            <div className="page flex justify-center items-center relative bg-white">
+            <div className="page flex justify-center items-center relative bg-black">
                 <video
                     className="absolute h-full w-full object-cover overflow-hidden object-top"
                     autoPlay
@@ -20,9 +34,9 @@ export const App = () => {
                     <source src={sample} type="video/mp4" />
                 </video>
 
-                <div className="h-20 rounded flex justify-center items-center z-20 w-1/3 md:w-full flex-col">
+                <div className="h-20 rounded flex justify-center items-center z-20 w-3/6 md:w-full flex-col">
                     <h1 className="text-6xl text-slate-50 select-none eefenjwfont">Bruiloft Eef & JW</h1>
-                    <h2 className="text-3xl text-slate-50 select-none mt-2 italic text-center w-full">
+                    <h2 className="text-3xl text-slate-50 select-none mt-2 italic w-full md:w-auto whitespace-nowrap">
                         nog <CountUp start={500} end={result} duration={3} /> dagen
                     </h2>
                 </div>
