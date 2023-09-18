9c6f32bd7c411b5beb186d20fc75914eb2dc9509
Rick H <rickhellenthal@hotmail.com>
2023-09-18 15:25:46 +0200

    Count down, fixes for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
+import CountUp from "react-countup";
 import "./App.css";
 
 //@ts-ignore
-import sample from "./assets/background.mp4";
+import sample from "./assets/background1080.mp4";
 import { differenceInDays } from "date-fns";
 
 export const App = () => {
     const result = differenceInDays(new Date(2024, 3, 19), new Date());
-
     return (
-        <div className="h-screen flex justify-center items-center relative">
-            <video className="absolute h-screen w-screen object-cover" autoPlay loop muted>
-                <source src={sample} type="video/mp4" />
-            </video>
+        <>
+            <div className="page flex justify-center items-center relative bg-white">
+                <video
+                    className="absolute h-full w-full object-cover overflow-hidden object-top"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                    disablePictureInPicture>
+                    <source src={sample} type="video/mp4" />
+                </video>
 
-            <div className="h-20 rounded flex justify-center items-center z-20 w-1/3 md:w-full flex-col">
-                <h1 className="text-6xl text-slate-50 select-none">Bruiloft Eef & JW</h1>
-                <h2 className="text-4xl text-slate-50 select-none mt-2">over {result} dagen</h2>
+                <div className="h-20 rounded flex justify-center items-center z-20 w-1/3 md:w-full flex-col">
+                    <h1 className="text-6xl text-slate-50 select-none eefenjwfont">Bruiloft Eef & JW</h1>
+                    <h2 className="text-3xl text-slate-50 select-none mt-2 italic text-center w-full">
+                        nog <CountUp start={500} end={result} duration={3} /> dagen
+                    </h2>
+                </div>
             </div>
-        </div>
+            {/* <div className="page ">
+                <h1 className="text-6xl select-none">Test</h1>
+            </div> */}
+        </>
     );
 };
 
