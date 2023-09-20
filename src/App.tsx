26f6138d6c16d767c4dea6bcc1ba1508a580692a
Rick H <rickhellenthal@hotmail.com>
2023-09-20 12:06:46 +0200

    fallback attempt

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,27 @@ import "./App.css";
 //@ts-ignore
 import sample from "./assets/background1080.mp4";
 import { differenceInDays } from "date-fns";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
+// TODO: Merge other laptop branch
 export const App = () => {
+    const [canPlay, setCanPlay] = useState(undefined);
+
     const setViewHeight = () => {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    const checkIfBackgroundVideoIsPlaying = () => {
+        setTimeout(() => {
+            var vid = document.getElementById("background-video-id");
+            if (vid) {
+                //@ts-ignore
+                console.log(vid.paused);
+                //@ts-ignore
+                setCanPlay(!vid.paused);
+            }
+        }, 750);
     };
 
     useEffect(() => {
@@ -18,21 +33,25 @@ export const App = () => {
         });
 
         setViewHeight();
+        checkIfBackgroundVideoIsPlaying();
     }, []);
 
     const result = differenceInDays(new Date(2024, 3, 19), new Date());
     return (
         <>
-            <div className="page flex justify-center items-center relative bg-black">
-                <video
-                    className="absolute h-full w-full object-cover overflow-hidden object-top"
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                    disablePictureInPicture>
-                    <source src={sample} type="video/mp4" />
-                </video>
+            <div className={`page flex justify-center items-center relative bg-black ${canPlay ? "" : "ankeveen-bg"}`}>
+                {canPlay !== false && (
+                    <video
+                        id="background-video-id"
+                        className="absolute h-full w-full object-cover overflow-hidden object-top"
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                        disablePictureInPicture>
+                        <source src={sample} type="video/mp4" />
+                    </video>
+                )}
 
                 <div className="h-20 rounded flex justify-center items-center z-20 w-3/6 md:w-full flex-col">
                     <h1 className="text-6xl text-slate-50 select-none eefenjwfont">Bruiloft Eef & JW</h1>
